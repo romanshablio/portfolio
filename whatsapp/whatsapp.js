f46749f1bc5ef2aa409b4bc0d5901fbd726514d4
romanshablio <59866804+romanshablio@users.noreply.github.com>
2021-06-15 14:59:57 +0300

    Add files via upload
    Маленькое приложение, будет полезно для кадровиков, чтобы не сохранять каждый раз номер в контактах телефона для передачи сообщений через WhatsApp

---
+++ whatsapp/whatsapp.js
@@ -0,0 +1,12 @@
+function send_handle(){
+
+    let num=document.getElementById("number").value;
+  
+    let msg= document.getElementById("msg").value;
+  
+      let name= document.getElementById("name").value;
+    
+    var win = window.open(`https://wa.me/${num}?text=I%27m%20api%20msg%20hello%20${name}%20friend%20${msg}`, '_blank');
+   win.focus();
+  }
+  
